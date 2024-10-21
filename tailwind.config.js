8dae9ab63ef444ef91a62727bee5a4d7d558876c
MihaiSebastian <mburghele@iesebre.com>
2024-10-21 20:28:06 +0200

    Push diari

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}"],
+  content: ['./components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
