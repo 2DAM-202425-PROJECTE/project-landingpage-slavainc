8381bd4a4c148ec5ebf30c77f62f55a50c3cefc4
Wharvey123 <hglover@iesebre.com>
2024-10-21 20:27:08 +0200

    Inici de hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}"],
+  content: ['./components//*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',],
   theme: {
     extend: {},
   },
