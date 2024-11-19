0f35090d9c60de9f44a19bf3889c07f899c4391c
Steven <mromero1@iesebre.com>
2024-11-19 15:34:04 +0100

    header acabat

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 termes: resolve(__dirname, 'pages/termes.html'),
                 privacitat: resolve(__dirname, 'pages/privacitat.html'),
                 avis_legal: resolve(__dirname, 'pages/avis_legal.html'),
+                contacte: resolve(__dirname, 'pages/contacte.html'),
             },
         },
     },
